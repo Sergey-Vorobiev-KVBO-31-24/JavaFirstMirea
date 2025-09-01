d3b01179a90a536326110bf805fdeb45bed2fa39
Sergey Vorobiev <sergeyvorobiew@mail.ru>
2025-09-01 17:46:19 +0300

    Add files via upload

---
+++ Practice1/src/ru/mirea/Main.java
@@ -0,0 +1,10 @@
+package ru.mirea;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Хэллоу ворлд! Лесссго!");
+    }
+}
