fe4dfe094e23341ecffe4ff70831f460ad42db13
Sergey Vorobiev <sergeyvorobiew@mail.ru>
2025-09-29 16:00:40 +0300

    Add files via upload

---
+++ Practice3/src/ru/mirea/taskFormatting/CurrencyConverter.java
@@ -0,0 +1,11 @@
+package ru.mirea.taskFormatting;
+
+import java.util.Locale;
+
+public class CurrencyConverter {
+    private Locale defaultLoc = Locale.
+
+    public static void main(String[] args) {
+        Locale.set
+    }
+}
